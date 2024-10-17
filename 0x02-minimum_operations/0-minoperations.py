42fca2582eeaf3e8fc15becfb5e6c2b688fce3ab
Babaoni147 <obahedwin@ymail.com>
2024-10-18 00:07:27 +0100

    Minimum Operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,27 +1,25 @@
 #!/usr/bin/python3
-'''Minimum Operations python3 challenge'''
-
+""" Module for 0-minoperations"""
 
 
 def minOperations(n):
-    '''Calculates the fewest number of operations needed to result in exactly n H characters.'''
-    
-    # If n is less than or equal to 1, we return 0 because it's impossible
-    if n <= 1:
+    """
+    minOperations
+    Gets fewest # of operations needed to result in exactly n H characters
+    """
+    # all outputs should be at least 2 char: (min, Copy All => Paste)
+    if (n < 2):
         return 0
-    
-    operations = 0
-    factor = 2  # Start with the smallest factor
-    
-    # Keep dividing n by its factors, starting with 2 and going up
-    while n > 1:
-        # While n is divisible by the current factor
-        while n % factor == 0:
-            # Add the factor to the operations count (because you copy and paste that many times)
-            operations += factor
-            # Divide n by this factor
-            n //= factor
-        # Move to the next factor
-        factor += 1
-    
-    return operations
+    ops, root = 0, 2
+    while root <= n:
+        # if n evenly divides by root
+        if n % root == 0:
+            # total even-divisions by root = total operations
+            ops += root
+            # set n to the remainder
+            n = n / root
+            # reduce root to find remaining smaller vals that evenly-divide n
+            root -= 1
+        # increment root until it evenly-divides n
+        root += 1
+    return ops
